88d6244e61f257f81ddfeca601bc824437b37dd3
Sachin-8125 <devinewolf7@gmail.com>
2025-09-13 03:29:53 +0530

    Raab Game 1 problem solved

--- Intro Problems/Raab_Game_I.cpp
+++ Intro Problems/Raab_Game_I.cpp
@@ -1,23 +1,46 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-// Function to calculate the number of possible game outcomes
-long long solve(int n, int a, int b) {
-  if (a == 0 && b == 0) return 1;
-  if (a < 0 || b < 0) return 0;
-  if (a > n || b > n) return 0;
-  return solve(n, a - 1, b) + solve(n, a, b - 1);
-}
-
 int main() {
-  int t;
-  cin >> t;
-  while (t--) {
-    int n, a, b;
-    cin >> n >> a >> b;
-    cout << solve(n, a, b) << endl;
-  }
-  return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        int n, a, b;
+        cin >> n >> a >> b;
+        if (a == 0 && b == 0) {
+            cout << "YES\n";
+            for (int i = 0; i < 2; i++) {
+                for (int j = 1; j <= n; j++) {
+                    cout << j << (j == n ? '\n' : ' ');
+                }
+            }
+            continue;
+        }
+        if (a + b > n || a == 0 || b == 0) {
+            cout << "NO\n";
+            continue;
+        }
+        cout << "YES\n";
+        int ties = n - a - b;
+        vector<int> xs(n + 1);
+        for (int i = 1; i <= a + b; i++) {
+            int pos = (i + b - 1) % (a + b) + 1;
+            xs[pos] = i;
+        }
+        
+        for (int i = a + b + 1; i <= n; i++) {
+            xs[i] = i;
+        }
+        for (int i = 1; i <= n; i++) {
+            cout << i << (i == n ? '\n' : ' ');
+        }
+        for (int i = 1; i <= n; i++) {
+            cout << xs[i] << (i == n ? '\n' : ' ');
+        }
+    }
+    return 0;
 }
