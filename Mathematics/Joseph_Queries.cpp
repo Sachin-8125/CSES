754633193d954c323f87989915e938343b7c463b
Sachin-8125 <devinewolf7@gmail.com>
2026-03-31 01:59:05 +0530

    Joseph Queries problem solved

--- Mathematics/Joseph_Queries.cpp
+++ Mathematics/Joseph_Queries.cpp
@@ -1,24 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
-int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        int n, k;
-        cin >> n >> k; // n = children, k = skip count
-        set<int> children;
-        for (int i = 1; i <= n; ++i)
-            children.insert(i);
-
-        int idx = 0;
-        while (!children.empty()) {
-            idx = (idx + k - 1) % children.size();
-            auto it = children.begin();
-            advance(it, idx);
-            cout << *it << " ";
-            children.erase(it);
-        }
-        cout << endl;
+int solve(int n, int k) {
+    if (n == 1) return 1;
+    if (k <= n / 2) return 2 * k;
+    
+    int res = solve(n - n / 2, k - n / 2);
+    
+    if (n % 2 == 0) {
+        return 2 * res - 1;
+    } else {
+        return res == 1 ? n : 2 * res - 3;
     }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int q;
+    if (cin >> q) {
+        while (q--) {
+            int n, k;
+            cin >> n >> k;
+            cout << solve(n, k) << "\n";
+        }
+    }
+
+    return 0;
 }
