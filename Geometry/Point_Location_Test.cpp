6255614758b284b9ca286a91e153d258cb9be7f7
Sachin-8125 <devinewolf7@gmail.com>
2026-04-28 20:43:47 +0530

    Point Location Test Problem Solved

---
+++ Geometry/Point_Location_Test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    long long x1, y1, x2, y2, x3, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+    long long cross_product = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+
+    if (cross_product > 0) {
+        cout << "LEFT" << endl;
+    } else if (cross_product < 0) {
+        cout << "RIGHT" << endl;
+    } else {
+        cout << "TOUCH" << endl;
+    }
+}
+
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
