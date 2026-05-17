0b0a5c6b88c2e17caa26924dc35d895b7b5c6e0b
Sachin-8125 <devinewolf7@gmail.com>
2026-05-17 21:44:34 +0530

    Increasing Subsequence problem solved

---
+++ Dynamic Programming/Increasing_Subsequence.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int& x : a) cin >> x;
+
+    vector<int> tails;
+
+    for (int x : a) {
+        auto it = lower_bound(tails.begin(), tails.end(), x);
+        if (it == tails.end())
+            tails.push_back(x);
+        else
+            *it = x;
+    }
+
+    cout << tails.size() << "\n";
+    return 0;
+}
